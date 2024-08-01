e08447faf265c29d79f969b916cbb7e39a77d544
Pepe <pepewee07@gmail.com>
2024-08-01 09:22:58 -0500

    change data

--- data.sql
+++ data.sql
@@ -282,7 +282,7 @@ INSERT INTO auth.users (email, name, last_name, phone_number, address, dni, pass
 VALUES ('pepewee07@gmail.com', 'José', 'Román', '0983439289', 'Av. Pumapungo y Renaciente', '0704713619', '$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6', true, true, true, true);
 
 INSERT INTO auth.users (email, name, last_name, phone_number, address, dni, password, enabled, account_non_expired, account_non_locked, credentials_non_expired)
-VALUES ('daniel@gmail.com', 'Daniel', 'Alvarez', '000000', 'Cuenca', 'V0002', '$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6', true, true, true, true);
+VALUES ('pepewee0@gmail.com', 'Pepe', 'Valdiviezo', '0983439289', 'Buenavista y Napoleon Mera', '0704713618', '$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6', true, true, true, true);
 
 -- Asignar roles a usuarios
 INSERT INTO auth.user_roles (user_id, role_id)
@@ -292,5 +292,5 @@ INSERT INTO auth.user_roles (user_id, role_id)
 SELECT (SELECT id FROM auth.users WHERE email = 'pepewee07@gmail.com'), (SELECT id FROM auth.roles WHERE name = 'USER');
 
 INSERT INTO auth.user_roles (user_id, role_id)
-SELECT (SELECT id FROM auth.users WHERE email = 'daniel@gmail.com'), (SELECT id FROM auth.roles WHERE name = 'USER');
+SELECT (SELECT id FROM auth.users WHERE email = 'pepewee0@gmail.com'), (SELECT id FROM auth.roles WHERE name = 'USER');
 
